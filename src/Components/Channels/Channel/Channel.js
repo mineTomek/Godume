9264c763edfa103592e7c6462abc4b4a3743cf67
mineTomek <lukom.tomek@gmail.com>
2022-10-30 09:15:53 +0100

    Procedural channels generation

--- src/Components/Channels/Channel/Channel.js
+++ src/Components/Channels/Channel/Channel.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Channel.css';
 
-function Channel() {
+function Channel(props) {
+
+    let channelTypeIconClass = 'solid fa-circle-exclamation Error';
+
+    if (props.type === 'text') {
+        channelTypeIconClass = 'regular fa-hashtag';
+    } else if (props.type === 'voice') {
+        channelTypeIconClass = 'solid fa-volume-high';
+    } else if (props.type === 'forum') {
+        channelTypeIconClass = 'regular fa-comments';
+    } else if (props.type === 'announcement') {
+        channelTypeIconClass = 'solid fa-bullhorn';
+    }
+
     return (
         <div className='Channel-box'>
-            # General
+            <i className={'fa-' + channelTypeIconClass + ' Channel-type'}></i> {props.name}
         </div>
     )
 }
